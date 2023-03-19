3969a3563efb90e4afd5687a8568d7e3c52883f2
stephfrankable <112651864+stephfrankable@users.noreply.github.com>
2023-03-19 14:07:41 -0500

    Uploading to repository

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "cryptoapi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "cryptofunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    // Create coins variable and set to empty array
+    const [coins, updateCoins] = useState([]);
 
-export default App;
+    // Create additional state to hold user input for limit and start properties
+    const [input, updateInput] = useState({ limit: 5, start: 0 });
+
+    // Create a variable for loading
+    const [loading, updateLoading] = useState(true);
+
+    // Create a new function to allow users to update the input values
+    function updateInputValues(type, value) {
+        updateInput({ ...input, [type]: value });
+    }
+
+    // Define function to call API
+    const fetchCoins = async() =>  {
+        updateLoading(true);
+        const { limit, start } = input;
+        const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`);
+        updateCoins(data.coins);
+        updateLoading(false);
+    }
+
+    // Call fetchCoins function when component loads
+    useEffect(() => {
+      fetchCoins()
+    }, [])
+
+    return (
+      <div className="App">
+        <input
+             placeholder="start"
+             onChange={e => updateInputValues('start', e.target.value)}
+         />
+         <input
+             onChange={e => updateInputValues('limit', e.target.value)}
+             placeholder="limit"
+         />
+         <button onClick={fetchCoins}>Fetch Coins</button>
+         {loading && <h2>Loading...</h2>}
+         {
+           !loading && coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
+  export default App
